808adfb63bc944b522e92fb5e8b747e3b5cd4874
Nahye Kim <egg4701@gmail.com>
2024-03-20 17:01:07 +0900

    a

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a=5, b=0;
+        while(b>a){
+            b++;
+        }
+    }
+}
